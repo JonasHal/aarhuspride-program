7f696cf0ad38fc05377a1e79167d7abde306f81c
JonasHal <60646377+JonasHal@users.noreply.github.com>
2025-05-25 20:33:25 +0200

    removed container

--- app.py
+++ app.py
@@ -297,7 +297,6 @@ def display_event_details(event):
     # Handle scroll action
     if st.session_state.scroll_to_top:
         scroll_to_here(50, key='top')  # 0ms for instant scroll
-        st.container(height=30)
         st.session_state.scroll_to_top = False
 
     # --- Back Button ---
